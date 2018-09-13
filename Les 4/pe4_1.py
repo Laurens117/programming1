d55260b989df7952def40654fdff0be5132b2554
Laurens <laurens.welsch@student.hu.nl>
2018-09-13 10:00:03 +0200

    overzicht moet nog gemaakt worden.

---
+++ Les 4/pe4_1.py
@@ -0,0 +1,8 @@
+cijferICOR=6
+cijferPROG=7
+cijferCSN=7
+cijfers= [cijferICOR, cijferPROG, cijferCSN]
+som = sum(cijfers)
+gemiddelde = som / len(cijfers)
+beloning = som *30
+print('beloning ' + str(beloning) )
