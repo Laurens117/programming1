93a14c9bd285495998f0cf0bdecc4bb1395c7782
Laurens <laurens.welsch@student.hu.nl>
2018-10-02 17:41:38 +0200

    er is geen if afstandkm >= abs(50): gebruikt.

--- Les 6/fa 6 ns.py
+++ Les 6/fa 6 ns.py
@@ -1,17 +1,20 @@
 def standaardprijs(afstandKM):
+    global prijs
     if 50 > afstandKM > 0:
-        prijs = afstandKM*0.80
+        prijs = afstandKM * 0.80
     elif afstandKM > 50:
-        prijs = 15+afstandKM*0.60
+        prijs = 15 + afstandKM * 0.60
     elif afstandKM < 0:
         prijs = 0
     return prijs
+
 
 def bepaal_weekend(dag):
     if dag == 'zaterdag' or dag == 'zondag':
         return True
     else:
         return False
+
 
 afstandKM = eval(input('Hoeveel km gaat u reizen?: '))
 dag = input('Welke dag gaat u reizen?: ')
@@ -20,18 +23,20 @@ leeftijd = int(input('Wat is uw leeftijd?: '))
 weekendrit = bepaal_weekend(dag)
 prijs = standaardprijs(afstandKM)
 
-def ritprijs (leeftijd, weekendrit, afstandKM):
+
+def ritprijs(leeftijd, weekendrit, afstandKM):
     if leeftijd < 12 or leeftijd >= 65:
         if weekendrit:
-            prijs2 = prijs * 0.65
+            nieuwe_prijs = prijs * 0.65
         else:
-            prijs2 = prijs * 0.70
+            nieuwe_prijs = prijs * 0.70
     else:
         if weekendrit:
-            prijs2 = prijs * 0.60
+            nieuwe_prijs = prijs * 0.60
         else:
-            prijs2 = prijs
-    return prijs2
+            nieuwe_prijs = prijs
+    return nieuwe_prijs
 
-prijs2 = ritprijs(leeftijd,weekendrit,afstandKM)
-print('U kunt €',prijs2,'betalen.')
+
+nieuwe_prijs = ritprijs(leeftijd, weekendrit, afstandKM)
+print('U kunt €', nieuwe_prijs, 'betalen.')
