1808143b6e8b83bc069127154bd83fb9032899d8
LaurensWelsch <laurens.welsch@student.hu.nl>
2018-10-15 20:20:16 +0200

    PE 7_2 def program.

--- Les 7/pe7_2.py
+++ Les 7/pe7_2.py
@@ -1,5 +1,7 @@
-filename = "kaartnummers"
-file = open(filename, "r")
-for line in file:
-    lst = line.split(",")
-    print('{} Heeft kaartnummer: {}'.format(lst[1].strip(),lst[0]))
+def split_txt(filename):
+    file = open(filename, "r")
+    for line in file:
+        lst = line.split(",")
+        print('{} Heeft kaartnummer: {}'.format(lst[1].strip(),lst[0]))
+
+split_txt('kaartnummers')
