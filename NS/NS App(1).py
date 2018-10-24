eb40ef1c544e3199fedd31e5fc2ab0baba46a44e
Laurens <laurens.welsch@student.hu.nl>
2018-10-24 16:01:06 +0200

    NS

--- NS/NS App(1).py
+++ NS/NS App(1).py
@@ -1,4 +1,6 @@
 from tkinter import *
+from NS_Api import *
+
 
 def raise_frame(frame):                                         #Met deze functie: wissel je van pagina frame
     frame.tkraise()
@@ -6,7 +8,7 @@ def raise_frame(frame):                                         #Met deze functi
 
 root = Tk()
 root.title("NS Automaat")
-root.geometry('600x800')
+root.geometry('1280x800')
 root.configure(bg="#f7d53d")
 
 
@@ -17,6 +19,7 @@ for frame in(Home, PageOne):
     frame.grid(row=0, column=0, sticky='news')
 
 Home.configure(bg="#f7d53d")
+PageOne.configure(bg='#f7d53d')
 
 logo = PhotoImage(file='logo.gif')
 logo_label = Label(Home,image= logo, bg="#f7d53d")
@@ -31,7 +34,12 @@ chipkaart_label.pack(padx=50, pady=10)
 Button(Home, text='Druk om verder te gaan',bg= "#1f0396", fg='white', command=lambda:raise_frame(PageOne)).pack()
 
 Label(PageOne, text='Kies een station', font= "Helvetica, 28", fg= "#1f0396", bg= "#f7d53d").pack()
-Button(PageOne, text='Stoppen', command=lambda:raise_frame(Home)).pack()
+Button(PageOne, text='Terug', command=lambda:raise_frame(Home)).pack()
+
+invoer=Label(PageOne, text='Voer een station in:').pack()
+entry_1= Entry(PageOne).pack()
+button_1=Button(PageOne, text='Enter').pack()
+
 
 raise_frame(Home)
 root.mainloop()
